37f3cd00f3e9573692d945585524879a82acc6ba
Vincent <vincent.vbonnet@gmail.com>
2025-07-30 19:31:41 -0400

    create or update

--- src/app/api/digest/[id]/route.ts
+++ src/app/api/digest/[id]/route.ts
@@ -5,9 +5,44 @@ import type { RawPassingInput } from '~/types/race-result-output';
 
 
 async function saveRawPassing(input: RawPassingInput) {
-    return db.rawPassing.create({
-        data: {
+    return db.rawPassing.upsert({
+        where: {
+            id: input.ID
+        },
+        create: {
             id: input.ID,
+            pid: input.PID,
+            timingPoint: input.TimingPoint,
+            result: input.Result,
+            time: input.Time,
+            invalid: input.Invalid,
+            bib: input.Bib,
+            transponder: input.Passing.Transponder,
+            hits: input.Passing.Hits,
+            rssi: input.Passing.RSSI,
+            battery: input.Passing.Battery,
+            temperature: input.Passing.Temperature,
+            wuc: input.Passing.WUC,
+            loopId: input.Passing.LoopID,
+            channel: input.Passing.Channel,
+            internalData: input.Passing.InternalData,
+            statusFlags: input.Passing.StatusFlags,
+            deviceId: input.Passing.DeviceID,
+            deviceName: input.Passing.DeviceName,
+            orderId: input.Passing.OrderID,
+            port: input.Passing.Port,
+            isMarker: input.Passing.IsMarker,
+            fileNo: input.Passing.FileNo,
+            passingNo: input.Passing.PassingNo,
+            customer: input.Passing.Customer,
+            received: new Date(input.Passing.Received),
+            utcTime: new Date(input.Passing.UTCTime),
+            latitude: input.Passing.Position.Latitude,
+            longitude: input.Passing.Position.Longitude,
+            altitude: input.Passing.Position.Altitude,
+            flag: input.Passing.Position.Flag
+        },
+        update: {
             pid: input.PID,
             timingPoint: input.TimingPoint,
             result: input.Result,
