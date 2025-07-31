57be11a887998c9cea02383855348ae25dd09cb7
Vincent <vincent.vbonnet@gmail.com>
2025-07-30 22:30:20 -0400

    init

--- prisma/migrations/20250731023011_init/migration.sql
+++ prisma/migrations/20250731023011_init/migration.sql
@@ -1,35 +1,5 @@
-/*
-  Warnings:
-
-  - You are about to drop the `Coureur` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `Equipe` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `Source` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `Tours` table. If the table is not empty, all the data it contains will be lost.
-
-*/
 -- CreateEnum
 CREATE TYPE "public"."Equipment" AS ENUM ('START', 'FINISH', 'DUAL', 'EXPERT', 'ROCKGARDEN', 'ONEORTWO');
-
--- DropForeignKey
-ALTER TABLE "public"."Coureur" DROP CONSTRAINT "Coureur_equipeId_fkey";
-
--- DropForeignKey
-ALTER TABLE "public"."Tours" DROP CONSTRAINT "Tours_coureurId_fkey";
-
--- DropTable
-DROP TABLE "public"."Coureur";
-
--- DropTable
-DROP TABLE "public"."Equipe";
-
--- DropTable
-DROP TABLE "public"."Source";
-
--- DropTable
-DROP TABLE "public"."Tours";
-
--- DropEnum
-DROP TYPE "public"."Borne";
 
 -- CreateTable
 CREATE TABLE "public"."Team" (
@@ -61,6 +31,17 @@ CREATE TABLE "public"."Lap" (
 );
 
 -- CreateTable
+CREATE TABLE "public"."LapEvent" (
+    "id" SERIAL NOT NULL,
+    "lapId" INTEGER NOT NULL,
+    "equipment" "public"."Equipment" NOT NULL,
+    "timestamp" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "bonusId" INTEGER,
+
+    CONSTRAINT "LapEvent_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "public"."Bonus" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
@@ -68,14 +49,6 @@ CREATE TABLE "public"."Bonus" (
     "points" INTEGER NOT NULL,
 
     CONSTRAINT "Bonus_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "public"."_BonusToLap" (
-    "A" INTEGER NOT NULL,
-    "B" INTEGER NOT NULL,
-
-    CONSTRAINT "_BonusToLap_AB_pkey" PRIMARY KEY ("A","B")
 );
 
 -- CreateIndex
@@ -91,10 +64,10 @@ CREATE UNIQUE INDEX "Participant_bib_key" ON "public"."Participant"("bib");
 CREATE INDEX "Lap_participantId_startTimestamp_idx" ON "public"."Lap"("participantId", "startTimestamp");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Bonus_name_key" ON "public"."Bonus"("name");
+CREATE INDEX "LapEvent_lapId_equipment_idx" ON "public"."LapEvent"("lapId", "equipment");
 
 -- CreateIndex
-CREATE INDEX "_BonusToLap_B_index" ON "public"."_BonusToLap"("B");
+CREATE UNIQUE INDEX "Bonus_name_key" ON "public"."Bonus"("name");
 
 -- AddForeignKey
 ALTER TABLE "public"."Participant" ADD CONSTRAINT "Participant_teamId_fkey" FOREIGN KEY ("teamId") REFERENCES "public"."Team"("id") ON DELETE SET NULL ON UPDATE CASCADE;
@@ -103,7 +76,7 @@ ALTER TABLE "public"."Participant" ADD CONSTRAINT "Participant_teamId_fkey" FORE
 ALTER TABLE "public"."Lap" ADD CONSTRAINT "Lap_participantId_fkey" FOREIGN KEY ("participantId") REFERENCES "public"."Participant"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "public"."_BonusToLap" ADD CONSTRAINT "_BonusToLap_A_fkey" FOREIGN KEY ("A") REFERENCES "public"."Bonus"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "public"."LapEvent" ADD CONSTRAINT "LapEvent_lapId_fkey" FOREIGN KEY ("lapId") REFERENCES "public"."Lap"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "public"."_BonusToLap" ADD CONSTRAINT "_BonusToLap_B_fkey" FOREIGN KEY ("B") REFERENCES "public"."Lap"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "public"."LapEvent" ADD CONSTRAINT "LapEvent_bonusId_fkey" FOREIGN KEY ("bonusId") REFERENCES "public"."Bonus"("id") ON DELETE SET NULL ON UPDATE CASCADE;
