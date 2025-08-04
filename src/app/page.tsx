3697511564c2c801595cb67ad5d43e6e90850953
Vincent <vincent.vbonnet@gmail.com>
2025-08-04 09:27:35 -0400

    remove menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,11 @@ export default async function Home() {
   <main className="flex min-h-screen flex-col items-center justify-center bg-gray-100 text-black">
       <nav className="flex w-full justify-center py-4">
         <ul className="flex gap-6 rounded-lg bg-white/100 px-6 py-2 text-lg font-semibold shadow">
-          <li>
+          {/* <li>
             <Link href="/individual" className="hover:underline">
               Individuel
             </Link>
-          </li>
+          </li> */}
           <li>
             <Link href="/teams" className="hover:underline">
               Teams
